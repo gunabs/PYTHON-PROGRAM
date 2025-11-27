95b08ac30ab490895e4cd6ae8779958c200d54a0
Guna.B <114652160+gunabs@users.noreply.github.com>
2025-11-27 15:08:30 +0530

    Simple Calculator

---
+++ BASIC/basic-1.py
@@ -0,0 +1,14 @@
+a=(int)(input("enter a num ="))
+b=(int)(input("enter a num ="))
+fun=(int)(input("enter a function 1.add 2.sub 3.mul 4.div "))
+
+if fun==1:
+    print(a+b)
+elif fun==2:
+    print(a-b)
+elif fun==3: 
+    print(a*b)
+else:
+    print(a/b)
+
+
