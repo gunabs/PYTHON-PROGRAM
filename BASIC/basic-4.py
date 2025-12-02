2ceac05acf66932fe266e87678bdf4e6bd892551
Guna.B <114652160+gunabs@users.noreply.github.com>
2025-12-02 18:03:04 +0530

    Check prime number

---
+++ BASIC/basic-4.py
@@ -0,0 +1,12 @@
+num=int(input("enter a num "))
+
+check=True
+for i in range(2,num//2+1):
+    if(num%i==0):
+        check=False
+if(check):
+    print("yes num is palyndrom")
+else:
+    print("no num is not palyndrom")
+        
+        
