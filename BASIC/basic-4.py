484a33640cea8912e7b79ea9eaa74110900a3304
Guna.B <114652160+gunabs@users.noreply.github.com>
2025-12-02 18:15:08 +0530

    Update basic-4.py

--- BASIC/basic-4.py
+++ BASIC/basic-4.py
@@ -5,8 +5,8 @@ for i in range(2,num//2+1):
     if(num%i==0):
         check=False
 if(check):
-    print("yes num is palyndrom")
+    print("yes num is prime")
 else:
-    print("no num is not palyndrom")
+    print("no num is not prime")
         
         
